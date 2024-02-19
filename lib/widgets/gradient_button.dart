b105b02d2f599d8708661148857184b859101dfb
zzinLee <sujin951017@gmail.com>
2024-02-19 22:10:30 +0900

    Feat: 그래디언트 효과를 넣은 버튼 위젯 추가

---
+++ lib/widgets/gradient_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  final String buttonText;
+  final Function()? onClick;
+
+  const GradientButton({
+    super.key,
+    required this.buttonText,
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(45),
+            gradient: const LinearGradient(
+              colors: [
+                Colors.blue,
+                Colors.purple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 13,
+            ),
+            child: Text(
+              buttonText,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          top: 0,
+          left: 0,
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(45),
+              onTap: onClick,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
