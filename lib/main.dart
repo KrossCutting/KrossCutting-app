d627d391c040fe46009f2278066f52aeafd11f64
zzinLee <sujin951017@gmail.com>
2024-02-19 22:14:56 +0900

    Docs: 스플래시 라우터 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:krosscutting_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const App());
@@ -7,12 +8,14 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "KrossCutting",
       initialRoute: "/",
+      routes: {
+        "/": (context) => const SplashScreen(),
+      },
       theme: ThemeData(
         colorScheme: const ColorScheme.dark(
           background: Colors.black,
