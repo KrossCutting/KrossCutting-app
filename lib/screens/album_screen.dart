282b8b25bc74324f257d7e0c8140bd1384f6e8a3
zzinLee <sujin951017@gmail.com>
2024-02-20 15:49:14 +0900

    Docs: 앨범 스크린 기초 레이어 구현

--- lib/screens/album_screen.dart
+++ lib/screens/album_screen.dart
@@ -10,12 +10,42 @@ class AlbumScreen extends StatefulWidget {
 class _AlbumScreenState extends State<AlbumScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Album Page",
-          style: TextStyle(
-            fontSize: 30,
+    Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: screenSize.width * 0.05,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: const AssetImage("assets/images/logo_square.png"),
+                  height: screenSize.height * 0.3,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Album",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                    ),
+                    Icon(
+                      Icons.video_collection_rounded,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
