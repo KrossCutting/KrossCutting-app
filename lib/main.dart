ac5240ae65a2c0bafc3ad4cb4a62a2eb693a5895
sht02048 <sht02048@gmail.com>
2024-02-21 20:35:23 +0900

    Fix: 병합 충돌 해결

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "KrossCutting",
-      initialRoute: "/",
+      initialRoute: "/progress",
       routes: {
         "/": (context) => const SplashScreen(),
         "/permission": (context) => const PermissionScreen(),
