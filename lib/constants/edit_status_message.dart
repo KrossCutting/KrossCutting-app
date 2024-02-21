1ded6510268febe2268da327f323aac8ed1af122
sht02048 <sht02048@gmail.com>
2024-02-21 14:59:07 +0900

    Feat: 진척 스크린 구현

---
+++ lib/constants/edit_status_message.dart
@@ -0,0 +1,8 @@
+const Map<String, String> PROGRESS_MESSAGE = {
+  'START': "Starting Krosscutting",
+  'AUDIOS': "Extracting audio files...",
+  'FRAMES': "Exporting frames to analyze...",
+  'SINGLESHOT': "Motion detection in progress, please wait a moment...",
+  'EDITING': "Editing video, please wait...",
+  'EXPORTING': "Exporting video, please wait...",
+};
