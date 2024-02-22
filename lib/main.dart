34cde52173fef3b4414f64252d844bc2237ed64f
zzinLee <sujin951017@gmail.com>
2024-02-22 14:51:59 +0900

    Docs: main에 dotenv 로드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:krosscutting_app/provider/video_path_provider.dart';
 import 'package:krosscutting_app/screens/album_screen.dart';
 import 'package:krosscutting_app/screens/home_page/home_page.dart';
 import 'package:krosscutting_app/screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
+
   runApp(const App());
 }
 
